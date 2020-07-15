a06913e30d52d428f9c4556027d4e1700e1ef082
Md Sakibul Islam <mdsakibulislamofficial@gmail.com>
2020-07-15 06:05:49 +0600

    error

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.send("Assalamu Walikum");
+  var options = { weekday: "long", day: "numeric", month: "long" };
+  var today = new Date();
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { mDay: day });
+  res.send();
 });
 
-app.listen(port, function () {
+app.post("/", function (req, res) {
+  console.log(req.body.newItem);
+});
+
+app.listen(3000, function () {
   console.log("server in up and running on port 3000");
 });
