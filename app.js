5f274ee7ac7d27e7dd9dfcc3c36027df2449091d
Md Sakibul Islam <mdsakibulislamofficial@gmail.com>
2020-07-25 05:27:30 +0600

    Rendering Itmems form  Database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true} ,{ useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/todolistDB', { useNewUrlParser: true });
 
 const app = express();
 
@@ -10,37 +10,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-const itemSchema  = {
-  name : String
-} 
-var Item = mongoose.model("item",itemSchema);
+const itemSchema = {
+  name: String
+}
+var Item = mongoose.model("item", itemSchema);
 
 var item1 = new Item({
-  name : "code for 1 houre"
+  name: "code for 1 houre"
 });
 var item2 = new Item({
-  name : "eat breakfast"
+  name: "eat breakfast"
 });
 var item3 = new Item({
-  name : "go to sleep"
+  name: "go to sleep"
 });
 
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if (err) {
-    console.log(err);
-  }else{
-    console.log("added successfully");
-  }
-});
+
 
 // item.save();
 
 
+
+
 app.get("/", function (req, res) {
-  res.render("list", { itemTitle: "Today", newListItems: items });
+
+
+  let itemArray = [];
+  Item.find(function (err, result) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      for (let i = 0; i < result.length; i++) {
+        itemArray.push(result[i].name);
+
+      }
+    }
+
+    if (result.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("added successfully");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { itemTitle: "Today", newListItems: itemArray });
+    }
+
+  });
+
 });
 
 app.post("/", function (req, res) {
